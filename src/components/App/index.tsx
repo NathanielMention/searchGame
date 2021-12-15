c38635fc27fb621242050e815c669604094d114e
Nathaniel Mention <nathanielmention@gmail.com>
2021-12-15 00:40:21 -0500

    fix tsconfig/ add app,home comp

---
+++ src/components/App/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from 'components/Home'
+
+const App = (): ReactElement => (
+	<>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</>
+)
+
+export default App
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,5 @@
+import React, { ReactElement } from 'react'
+
+const Home = (): ReactElement => <main>Home</main>
+
+export default Home
